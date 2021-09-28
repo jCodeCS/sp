59a9de83a9383fb6d6cfcbde19c9a75a0e736936
jCodeCS <91510334+jCodeCS@users.noreply.github.com>
2021-09-27 20:41:46 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello World")
+item = 1
+print(item)
+name = game.ask_for_string("Enter your name")
+print(name)
---
+++ main.ts
@@ -0,0 +1,5 @@
+console.log("Hello World")
+let item = 1
+console.log(item)
+let name = game.askForString("Enter your name")
+console.log(name)
